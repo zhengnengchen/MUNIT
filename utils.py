76219a6d4fdb652adc7403b5ea1cf9079102e80a
Zhengneng Chen <czn9689@gmail.com>
2019-06-24 10:08:49 -0400

    Fix attribute error in train.py and yaml.load warning in utils.py

--- utils.py
+++ utils.py
@@ -99,7 +99,7 @@ def get_data_loader_folder(input_folder, batch_size, train, new_size=None,
 
 def get_config(config):
     with open(config, 'r') as stream:
-        return yaml.load(stream)
+        return yaml.full_load(stream)
 
 
 def eformat(f, prec):
